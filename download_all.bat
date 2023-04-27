4d1ddfe8624598e9ebebabdb53b3dd1bed3a6fab
KGowthamTN <90371239+KGowthamTN@users.noreply.github.com>
2023-04-27 13:23:20 -0700

    Add files via upload

---
+++ download_all.bat
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/01_nfa.c -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/Nfa_ip.txt -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/02_dfa.c -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/Dfa_ip.txt -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/05_postfix_expression.c -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/06_symbol_table.c -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/08_shift_reduce_parsing.c -O
+curl https://raw.githubusercontent.com/KGowthamTN/Compiler_Design_Lab/main/11_quadruples.c -O
